bc2193b79b0fe2c1e0d6eb986280d0c331e86db6
kellz <esquejokelly@gmail.com>
2024-08-06 12:29:34 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect } from 'react'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+
+import Sidenav from './components/Sidenav'
+
+import './App.css'
+
+
+function App() {
+	useEffect(() => {
+		const handleWheel = (event) => {
+			const container = document.querySelector('.images-container');
+			if (container && event.deltaY !== 0) {
+				container.scrollLeft += event.deltaY;
+				event.preventDefault(); // Prevent default vertical scrolling behavior
+			}
+		};
+
+		window.addEventListener('wheel', handleWheel, { passive: false });
+
+		// Clean up the event listener on component unmount
+		return () => {
+			window.removeEventListener('wheel', handleWheel);
+		};
+	}, []);
+
+	return (
+		<div className='whole-page'>
+			<BrowserRouter>
+				<Sidenav/>
+					<Routes>  
+						<Route path="/" element={<Home />}>
+							<Route index element={<Home />} />
+							<Route index path="about" element={<About/>} />
+							<Route path="contact" element={<Contact />} />
+							<Route path="work" element={<Projects />} />
+						</Route>
+					</Routes>
+			</BrowserRouter>
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import linkedInLogo from '/linkedin-logo.svg'
+import gitHubLogo from '/github-mark.svg'
+import "../css/sidenav.css"
+import { Link } from "react-router-dom";
+
+
+function Sidenav() {
+  return (
+    <div className='sidebar'>
+        <div className='title'>
+           Kelly Esquejo Portfolio
+        </div>
+        <div className=''>
+            <a href="https://github.com/esquek1" target="_blank">
+                <img src={gitHubLogo} className="logo" alt="GitHub logo" />
+            </a>
+            <a href="https://www.linkedin.com/in/kelly-esquejo/" target="_blank">
+                <img src={linkedInLogo} className="logo linkedin" alt="LinkedIn logo"/>
+            </a>
+        </div>
+        <div className='pages'>
+            <nav >
+                <ul className='nav-list'>
+                    <li className='nav-item'>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li className="nav-item extraBorder">
+                        <Link to="/work">Projects</Link>
+                    </li>
+                </ul>
+                
+            </nav>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Sidenav
