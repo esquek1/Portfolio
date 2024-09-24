a006558e04689d6ee363a56a99529b6c703071ee
esquek1 <esquejokelly@gmail.com>
2024-09-23 20:13:04 -0700

    Change layout using scroll parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,79 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+
 import Projects from './pages/Projects';
 
 import Sidenav from './components/Sidenav'
 import './App.css'
 
-
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 function App() {
-	useEffect(() => {
-		const handleWheel = (event) => {
-			const container = document.querySelector('.images-container');
-			if (container && event.deltaY !== 0) {
-				container.scrollLeft += event.deltaY;
-				event.preventDefault(); // Prevent default vertical scrolling behavior
-			}
-		};
+	// useEffect(() => {
+	// 	const handleWheel = (event) => {
+	// 		const container = document.querySelector('.images-container');
+	// 		if (container && event.deltaY !== 0) {
+	// 			container.scrollLeft += event.deltaY;
+	// 			event.preventDefault(); // Prevent default vertical scrolling behavior
+	// 		}
+	// 	};
 
-		window.addEventListener('wheel', handleWheel, { passive: false });
+	// 	window.addEventListener('wheel', handleWheel, { passive: false });
 
-		// Clean up the event listener on component unmount
-		return () => {
-			window.removeEventListener('wheel', handleWheel);
-		};
-	}, []);
+	// 	// Clean up the event listener on component unmount
+	// 	return () => {
+	// 		window.removeEventListener('wheel', handleWheel);
+	// 	};
+	// }, []);
+	const ref = useRef();
 
 	return (
-		<div className='whole-page'>
-			<BrowserRouter>
-				<Sidenav/>
+		// <div className='whole-page'>
+		// 	<BrowserRouter>
+		// 		<Sidenav/>
 				
-				<Routes>  
-					<Route index element={<Home />} />
-					<Route index path="about" element={<About/>} />
-					<Route path="contact" element={<Contact />} />
-					<Route path="work" element={<Projects />} />
-				</Routes>
-			</BrowserRouter>
+		// 		<Routes>  
+		// 			<Route index element={<Home />} />
+		// 			<Route index path="about" element={<About/>} />
+		// 			<Route path="contact" element={<Contact />} />
+		// 			<Route path="projects" element={<Projects />} />
+		// 		</Routes>
+		// 	</BrowserRouter>
+		// </div>
+		<div>
+			<Parallax pages={4} ref={ref} style={{ top: '0', left: '0' }}>
+
+				<ParallaxLayer >
+
+					<img />
+					<Home/>
+				</ParallaxLayer>
+
+				<ParallaxLayer 
+					offset={1}
+					onClick={() => ref.current.scrollTo(1)}
+				>
+					<Contact/>
+				</ParallaxLayer>
+
+				<ParallaxLayer 
+					offset={2}
+					onClick={() => ref.current.scrollTo(2)}
+				>
+					<About />
+				</ParallaxLayer>
+
+				<ParallaxLayer 
+					offset={3}
+					onClick={() => ref.current.scrollTo(3)}
+				>
+					<Projects/>
+				</ParallaxLayer>
+
+			</Parallax>
 		</div>
 	)
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-
+import '../css/projects.css'
 function Projects() {
   return (
     <div className='projects-container'>
-      <div className='project-item'>
-        project 1
-      </div>
-      <div className='project-item'>
-        project 2
-      </div>
-      <div className='project-item'>
-        project 3
-      </div>
+		<h3>Projects </h3>
+		<div className='project-item'>
+			project 1
+		</div>
+		<div className='project-item'>
+			project 2
+		</div>
+		<div className='project-item'>
+			project 3
+		</div>
+		<div className='project-item'>
+			project 4
+		</div>
 
     </div>
   )
