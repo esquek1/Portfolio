e7138ad069e70e9eb95306c7a0b6769ebd11b048
esquek1 <esquejokelly@gmail.com>
2025-01-24 21:28:20 -0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useEffect, useRef, useState } from "react";
-import ReactDOM from "react-dom/client";
-
 import Terminal from "./pages/Terminal";
 import Background from "./components/Background";
 
-import "./css/app.css";
+import "./App.css";
 
 function App() {
     return (
