7fcf71dd7391f01efafa63756f7edaf1f80a30c6
esquek1 <esquejokelly@gmail.com>
2025-02-14 11:46:01 -0800

    Project addition: kellz.land

--- src/pages/Terminal.jsx
+++ src/pages/Terminal.jsx
@@ -4,7 +4,6 @@ import About from "../components/About";
 import Contact from "../components/Contact";
 import Projects from "../components/Projects";
 import Jokes from "../components/Joke";
-import { BsCapslockFill } from "react-icons/bs";
 
 const COMMANDS = [
     { cmd: "-about", desc: "About Me", component: <About /> },
